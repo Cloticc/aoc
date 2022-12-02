a16d5c8feb8752269abc6fd5c5972d282722c4ea
Clotic <isidorssona@gmail.com>
2022-12-02 19:39:18 +0100

    Day2Lua

---
+++ 2022/Day 2/code.lua
@@ -0,0 +1,48 @@
+-- X = lose 0
+-- Y = draw 3
+-- Z = win 6
+-------------------------
+-- A = Rock
+-- B = Paper
+-- C = Scissors
+--------------------------
+-- D = Rock
+-- E = Paper
+-- F = Scissors
+
+
+
+local scores = {
+    ["A"] = {
+        ["X"] = 3,
+        ["Y"] = 4,
+        ["Z"] = 8
+    },
+    ["B"] = {
+        ["X"] = 1,
+        ["Y"] = 5,
+        ["Z"] = 9
+    },
+    ["C"] = {
+        ["X"] = 2,
+        ["Y"] = 6,
+        ["Z"] = 7
+    }
+}
+
+function main()
+    local file = io.open("input.txt", "r")
+    local totalScore = 0
+    if file then
+        for line in file:lines() do
+            local opponent, you = line:match("(%w) (%w)")
+
+
+            totalScore = totalScore + scores[opponent][you]
+        end
+
+        print(totalScore)
+    end
+end
+
+main()
