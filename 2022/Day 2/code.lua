bbacdf5ca7ef69ae63b23c7be56d15954d68cc3c
Clotic <isidorssona@gmail.com>
2022-12-02 19:40:54 +0100

    add first part of the riddle

--- 2022/Day 2/code.lua
+++ 2022/Day 2/code.lua
@@ -1,17 +1,22 @@
--- X = lose 0
--- Y = draw 3
--- Z = win 6
--------------------------
--- A = Rock
--- B = Paper
--- C = Scissors
---------------------------
--- D = Rock
--- E = Paper
--- F = Scissors
-
-
-
+-- --First
+-- local scores = {
+--     ["A"] = {
+--         ["X"] = 4,
+--         ["Y"] = 8,
+--         ["Z"] = 3
+--     },
+--     ["B"] = {
+--         ["X"] = 1,
+--         ["Y"] = 5,
+--         ["Z"] = 9
+--     },
+--     ["C"] = {
+--         ["X"] = 7,
+--         ["Y"] = 2,
+--         ["Z"] = 6
+--     }
+-- }
+--Second
 local scores = {
     ["A"] = {
         ["X"] = 3,
@@ -37,7 +42,6 @@ function main()
         for line in file:lines() do
             local opponent, you = line:match("(%w) (%w)")
 
-
             totalScore = totalScore + scores[opponent][you]
         end
 
@@ -46,3 +50,15 @@ function main()
 end
 
 main()
+
+-- X = lose 0
+-- Y = draw 3
+-- Z = win 6
+-------------------------
+-- A = Rock
+-- B = Paper
+-- C = Scissors
+--------------------------
+-- D = Rock
+-- E = Paper
+-- F = Scissors
